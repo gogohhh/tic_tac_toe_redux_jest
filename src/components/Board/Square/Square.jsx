12d70d055dc4b0ab772e1d87bae580522e74ded2
gogohhh <gogo_hhh_d@hotmail.com>
2021-11-19 15:29:54 -0600

    Cambios de turno

--- src/components/Board/Square/Square.jsx
+++ src/components/Board/Square/Square.jsx
@@ -4,9 +4,12 @@ import Circle from "./Circle.jsx";
 import '../../../App.css';
 
 function Square(props){
+    const {turno} = props
+
     return (
+        
         <div className="cell m-0">
-           {props.turno}  {/*Ahora se pasa solo el turno y los square quedan nulos */} 
+           {turno ? (turno === 'X' ? <Cross /> : <Circle /> ) : '' }  {/*Ahora se pasa solo el turno y los square quedan nulos */} 
         </div>
     )
     
