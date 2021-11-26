9bdc4c899e36b6fff117631a0ce8dad767030648
gogohhh <gogo_hhh_d@hotmail.com>
2021-11-26 10:04:55 -0600

    test para componentes

---
+++ src/__test__/testComponents/Tablero.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Tablero from '../../components/Tablero/Tablero';
+
+import { render, screen } from '@testing-library/react';
+
+test('Render tablero', () => {
+    render(<Tablero />);
+    const linkElement = screen.getByText(/Jugador 1:/i) || screen.getByText(/Jugador 2:/i);
+    expect(linkElement).toBeInTheDocument();
+  });
